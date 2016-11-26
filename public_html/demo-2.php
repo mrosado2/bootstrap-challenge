dc99af275422dd11782feba1b8dbcd6c25880d1b
Maria Rosado <mrosado2@cnm.edu>
2016-11-26 16:52:43 -0700

    divs

---
+++ public_html/demo-2.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php require_once("lib/head-utils.php");?>
+	</head>
+	<body class="footer">
+		<div class="sfooter">
+			<!-- begin header -->
+			<header>
+				<nav class="navbar navbar-defaul">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Bootstrap Demo 2</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Link 1</a></li>
+								<li><a href="#">Link 2</a></li>
+								<li><a href="#">Link 3</a></li>
+								<li><a href="#">Link 4</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div>
+				</nav>
+			</header>
+		</div>
+
+		<!-- main content section -->
+		<main>
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="jumbotron">
+							<h1>Welcome! :D</h1>
+							<p>Scamper my slave human didn't give me any food so i pooped on the floor meow to be let in.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="box text-center">
+							text area 1
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="box text-center">
+							text area 2
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="box text-center">
+							text area 3
+						</div>
+					</div>
+				</div><!-- /.row -->
+			</div><!-- /.container-fluid -->
+		</main>
+		</div>
+
+		<footer>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-6">
+						<small>This is the foooter</small>
+					</div>
+					<div class="col-sm-6 text-right">
+						<small>Link 1  |  Link 2</small>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
