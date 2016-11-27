5a39d26b904d6db597ff087dbe1afc9ae15e73d6
Maria Rosado <mrosado2@cnm.edu>
2016-11-26 17:00:39 -0700

    added files

---
+++ public_html/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php require_once("lib/head-utils.php");?>
+	</head>
+	<body class="home-page">
+		<div class="container">
+
+			<!-- welcome section -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Welcome!</h1>
+				</div>
+			</div>
+
+			<!-- first content area -->
+			<div class="row">
+				<div class="col-md-6">
+					text area 1
+				</div>
+				<div class="col-md-6">
+					test area 2
+				</div>
+			</div>
+
+			<!-- second content area -->
+			<div class="row">
+				<div class="col-md-3">
+					text area 3
+				</div>
+				<div class="col-md-3">
+					text area 4
+				</div>
+				<div class="col-md-3">
+					text area 5
+				</div>
+				<div class="col-md-3">
+					text area 6
+				</div>
+			</div>
+			<div class="row text-center">
+				<div class="col-xs-12">
+					<p><a href="demo-2.php">Next Page >>></a></p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+	</body>
+</html>
